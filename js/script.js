f52a8252f75b2293db60b4878d3f3e0032ac3fb7
Anton <makar.toxa@gmail.com>
2022-07-14 17:40:54 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,147 @@
+// // console.log(1);
+
+// "use strict";
+
+// a = 15;
+// console.log(a);
+
+
+// console.log(number)
+// let number = 5;
+
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// console.log(name);
+// var name = 'Ivan';
+
+
+// // {
+// // 	var result = 50;
+// // }
+
+// // console.log(result);
+// const vehicleBodyWidth = 5000;
+// const vehicleBodyLenght = 4000;
+// console.log("Ширина автомобиля: " + vehicleBodyWidth + ', длина: ' + vehicleBodyLenght)
+
+
+// // ['vruru', 'ewfbg', 'abkhc'].map(a => );
+
+// // // Request
+// // data
+
+
+// // snake_case
+// // UPPER_SNAKE_CASE
+// // Kebab-case
+// //PascalCase
+
+// let COLOR_RED = '#F00';
+
+// let number = 4.6;
+
+// console.log(-4/0);
+// console.log('string' * 9);
+
+// const persone = `Alex`;
+
+// const bool = false;
+// // console.log(something); null
+// let und;
+// console.log(und);
+
+// <!--Oбьекты-->
+
+
+// const obj = {
+// 	name: "John",
+// 	age: 25,
+// 	isMarried: false
+// };
+// // console.log(obj.age);
+// console.log(obj["name"]);
+
+// <!-- Массивы -->
+
+
+// let arr = ['plum.pmg', 'orange.jpg', 6, 'apple.bmp', {}, []];
+// console.log(arr[1]);
+
+
+// <!--Разница между обьектами и массивами-->
+
+// const arr = ['a', 'b', 'c'];
+
+// arr[10] = '123445';
+// console.log(arr);
+
+// const arrObj = {
+// 	0: 'a',
+// 	1: 'b',
+// 	2: 'c',
+// 	abc: {
+// 		df: [{}, {}],
+// 		def: {
+
+// 		}
+
+// 	}
+// };
+
+// const b = [];
+// ;
+// arrObj.b = '1234';
+// arrObj['b'] = '1234';
+
+// console.log(arrObj['b']);
+// console.log(arrObj.b);
+
+// // const obj = {a: 1, b: 2};
+// const obj = {
+// 	Anna: 500,
+// 	'Alice': 800
+// };
+// console.log(obj.Anna);
+
+
+// <!--Простое общение с пользователем на базовом уровне-->
+
+// alert('Hello!);
+
+// const result = confirm("Are you here?");
+
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18?", "18");
+// console.log(typeof(answer));
+// console.log(answer +5);
+
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваше фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+
+// <!--Интерполяция-->
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Ivan";
+
+// alert(`Привет, ${user}`);
+
+
+// <!--Операторы в JS-->
+
+console.log('arr' + " - object");
+console.log(4 + +"5");
