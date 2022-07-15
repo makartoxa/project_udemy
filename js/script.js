79d0ebab9c4a9512b6a6beefc3db06d29948a116
Anton <makar.toxa@gmail.com>
2022-07-15 14:13:47 +0300

    add логиеские операторы

--- js/script.js
+++ js/script.js
@@ -221,3 +221,60 @@
 // }
 
 <!--Логические операторы-->
+
+// const hamburger = true;
+// const fries = true;
+
+// if (hamburger && fries) {
+// 	console.log('Я сыт!');
+// }
+
+// console.log((hamburger && fries));
+
+// const hamburger = 3;
+// const fries = 1;
+// const cola = 1
+
+// console.log(hamburger === 3 && cola && fries);
+
+// if (hamburger === 3 && cola === 1 && fries) {
+// 	console.log('Все сыты');
+// } else {
+// 	console.log('Мы уходим!')
+// }
+
+// console.log(1 && 0);
+// console.log(1 && 0 && undefined && 2);
+// console.log(null && 5);
+// console.log(0 && 'gfuygucg');
+
+// const hamburger = 1;
+// const fries = null;
+// const cola = 2;
+
+// if (hamburger || cola || fries) {
+// 	console.log('Все довольны');
+// } else {
+// 	console.log('Мы уходим!')
+// }
+
+// console.log(hamburger || cola || fries);
+
+// let johnReport, alexReport, samReport, mariaReport = 'done';
+
+// console.log(johnReport || alexReport || samReport || mariaReport);
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+
+// if (hamburger === 3 && (cola === 2 || fries === 3) && nuggets) {
+// 	console.log('Все довольны');
+// } else {
+// 	console.log('Мы уходим!')
+// }
+
+// console.log(hamburger === 3 && cola === 2 || fries === 3 && nuggets);
+
+// console.log(!0);
