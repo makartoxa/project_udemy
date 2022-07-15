9dd300908b354eacb698d6e372a08598907b56d0
Anton <makar.toxa@gmail.com>
2022-07-15 14:31:06 +0300

    update логиеские операторы

--- js/script.js
+++ js/script.js
@@ -278,3 +278,17 @@
 // console.log(hamburger === 3 && cola === 2 || fries === 3 && nuggets);
 
 // console.log(!0);
+// console.log( !1 && 2 || !3 );
+// console.log( 25 || null && !3 );
+// console.log( NaN || null || !3 || undefined || 5); 
+// console.log( 5 === 5 && 3 > 1 || 5);
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+ 
+// if (hamburger === 3 && cola || fries === 3 && nuggets) {
+//    console.log('Done!');
+// }
+
