456da731a66de0ee73781420ed73388b5b8d6326
Anton <makar.toxa@gmail.com>
2022-07-15 17:13:10 +0300

    update цыкл в цыкле и метки

--- js/script.js
+++ js/script.js
@@ -314,3 +314,44 @@
 // 	}
 // 	console.log(i);
 // }
+
+// <!--Цыкл в цыкле и метки-->
+
+// for (let i = 0; i < 3; i++) {
+// 	console.log(i);
+// 	for (let j = 0; j < 3; j++) {
+// 		console.log(j);
+// 	}
+// }
+
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+
+// let result ='';
+// const lenght = 7;
+
+// for (let i = 1; i < lenght; i++) {
+	
+// 	for (let j = 0; j < i; j++) {
+// 		result += "*";
+// 	}
+
+// 	result += '\n';
+// }
+
+// console.log(result);
+
+// first: for (let i = 0; i < 3; i++) {  // метки!!!!
+// 	console.log(`First level: ${i}`);
+// 	for (let j = 0; j < 3; j++) {
+// 		console.log(`Second level: ${j}`);
+// 		for (let k = 0; k < 5; k++) {
+// 			if (k === 2) continue first; // break!
+// 			console.log(`Third level: ${k}`);
+// 		}
+// 	}
+// }
