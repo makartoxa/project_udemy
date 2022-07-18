854268b116de495763ef2c4f04a6369e3a5ac716
Anton <makar.toxa@gmail.com>
2022-07-18 19:17:14 +0300

    Функции, стрелочные функции (ES6)

--- js/script.js
+++ js/script.js
@@ -507,4 +507,23 @@
 
 		// console.log(personalMovieDB);
 
-		
+		// <!--Функции, стрелочные функции (ES6)-->
+    
+		let num = 20;
+
+		function showFirstMessage(text) {
+			console.log(text);
+			let num = 10;
+			console.log(num);
+		}
+
+		showFirstMessage("Hello, World!");
+		console.log(num);
+
+		function calc(a, b) {
+			return(a + b);
+		}
+
+		console.log(calc(4, 3));
+		console.log(calc(5, 6));
+		console.log(calc(7, 9));
