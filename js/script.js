4545afd2a0491621a9cf23c17d2472d7f8afd05d
Anton <makar.toxa@gmail.com>
2022-07-18 15:02:13 +0300

    update task

--- js/script.js
+++ js/script.js
@@ -356,11 +356,101 @@
 // 	}
 // }
 
-let result = '';
-function firstTask() {
-	for (let i = 5; i <= 10; i++) {
-		console.log(i);
-		result += i;
-	}
-	console.log(result);
-}
+// <--Упражнения-->
+
+// for (let i = 2; i <= 16; i++) {
+// 	    if (i % 2 === 0) {
+// 	        continue;
+// 	    } else {
+// 	        console.log(i);
+// 	    }
+// 	}
+	// let i = 2;
+	// while (i <= 16) {
+	// 	if (i % 2 === 0) {
+	// 		i++;
+	// 		continue;
+	// 	} else {
+	// 		console.log(i);
+	// 	}
+	// 		i++;
+	// }
+
+// function fifthTask() {
+// 	const arrayOfNumbers = [];
+
+// 	// Пишем решение вот тут
+//  for (let i = 0; i <= 5; i++) {
+// 	arrayOfNumbers[i] = i + 5; 
+//  }
+// 	// Не трогаем
+// 	return arrayOfNumbers;
+// }
+
+// // Место для первой задачи
+// function firstTask() {
+// 	// Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+// 	const arr = [3, 5, 8, 16, 20, 23, 50];
+// 	const result = [];
+
+// 	// Пишем решение вот тут
+// 	for (let i = 0; i < arr.length; i++) {
+// 			result[i] = arr[i];
+// 	}
+// 	 console.log(result);
+// 	// Не трогаем
+// 	return result;
+// }
+
+// // Место для второй задачи
+// function secondTask() {
+// 	// Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+// 	const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+// 	// Пишем решение вот тут
+//  for (let i = 0; i < data.length; i++) {
+// 			if (typeof(data[i]) === 'number') {
+// 					data[i] = data[i] * 2;
+// 			} else if (typeof(data[i]) === 'string') {
+// 					data[i] = `${data[i]} - done`;
+// 			}
+// 	}
+
+// 	console.log(data);
+// 	// Не трогаем
+// 	return data;
+// }
+
+// // Место для третьей задачи
+// function thirdTask() {
+// 	// Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+// 	const data = [5, 10, 'Shopping', 20, 'Homework'];
+// 	const result = [];
+
+// 	// Пишем решение вот тут
+// 	for (let i = 0; i < data.length; i++) {
+// 			result[i] = data[(data.length - 1) - i]
+// 	}
+	
+// 	// Не трогаем
+// 	return result;
+// }
+
+
+// <--Пирамида-->
+
+// const lines = 5;
+// let result = '';
+
+// for (let i = 0; i <= lines; i++) {
+//     for (let j = 0; j < lines - i; j++) {
+//         result += " ";
+//     }
+//     for (let j = 0; j < 2 * i + 1; j++) {
+//         result += "*";
+//     }
+//     result += "\n";
+// }
+
+// console.log(result);
+
