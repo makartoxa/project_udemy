146939ddb87c2cbe665fb81b34bb6a74ba03be6e
Anton <makar.toxa@gmail.com>
2022-07-20 18:28:05 +0300

    Метод Фибоначчи

--- js/script.js
+++ js/script.js
@@ -825,30 +825,30 @@
 
 // <!--Метод Фибоначчи-->
 
-function fib(num) {
-	if (typeof(num) !== 'number' || num <= 0 || !Number.isInteger(num)) {
-			return "";
-	}
+// function fib(num) {
+// 	if (typeof(num) !== 'number' || num <= 0 || !Number.isInteger(num)) {
+// 			return "";
+// 	}
 
-	let result = '';
-	let first = 0;
-	let second = 1;
+// 	let result = '';
+// 	let first = 0;
+// 	let second = 1;
 
-	for (let i = 0; i < num; i++) {
-			if (i + 1 === num) {
-					result += `${first}`;
-					// Без пробела в конце
-			} else {
-					result += `${first} `;
-			}
+// 	for (let i = 0; i < num; i++) {
+// 			if (i + 1 === num) {
+// 					result += `${first}`;
+// 					// Без пробела в конце
+// 			} else {
+// 					result += `${first} `;
+// 			}
 
-			let third = first + second;
-			first = second;
-			second = third;
-	}
+// 			let third = first + second;
+// 			first = second;
+// 			second = third;
+// 	}
 
-	return result;
-}
+// 	return result;
+// }
 
-fib(5);
+// fib(5);
 
