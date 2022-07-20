47c16f2e6c458090e494f8cad804d2e1dadf1984
Anton <makar.toxa@gmail.com>
2022-07-20 17:43:20 +0300

    Упражнение по написанию кода Продвинутые задания на использование функций

--- js/script.js
+++ js/script.js
@@ -777,3 +777,49 @@
 // 	} 
 // }
 // console.log(getCoupeNumber(33));
+
+// <!--Упражнение по написанию кода Продвинутые задания на использование функций-->
+
+
+// function getTimeFromMinutes(minutesTotal) {
+// 	if (typeof(minutesTotal) !== 'number' || minutesTotal < 0 || !Number.isInteger(minutesTotal)) {
+// 			return "Ошибка, проверьте данные";
+// 	}
+
+// 	const hours = Math.floor(minutesTotal / 60);
+// 	const minutes = minutesTotal % 60;
+
+// 	let hoursStr = '';
+
+// 	switch (hours) {
+// 			case 0: 
+// 					hoursStr = 'часов';
+// 					break;
+// 			case 1:
+// 					hoursStr = 'час';
+// 					break;
+// 			case 2:
+// 			case 3:
+// 			case 4:
+// 					hoursStr = 'часа';
+// 					break;
+// 			default:
+// 					hoursStr = 'часов';
+// 	}
+
+// 	return `Это ${hours} ${hoursStr} и ${minutes} минут`;
+// }
+
+// getTimeFromMinutes(180);
+
+// function findMaxNumber(a, b ,c, d) {
+// 	if (typeof(a) !== 'number' ||
+// 			typeof(b) !== 'number' ||
+// 			typeof(c) !== 'number' ||
+// 			typeof(d) !== 'number' ) {
+// 				return 0;
+// 			}
+// 	return Math.max(a, b ,c, d);
+// }
+// console.log(findMaxNumber(1, 2, 10, 7));
+
