e56037aa4f7f503f4bd1798f5429dded19ab9fc4
Anton <makar.toxa@gmail.com>
2022-07-21 12:52:16 +0300

    Объекты, деструктуризация объектов (ES6)

--- js/script.js
+++ js/script.js
@@ -957,3 +957,63 @@
 
 // learnJS('JavaScript', done);
 
+
+// <!--Объекты, деструктуризация объектов (ES6)-->
+
+// const options = {
+// 	name: 'test',
+// 	width: 1024,
+// 	height: 1024,
+// 	colors: {
+// 		border: 'black',
+// 		bg: 'red'
+// 	},
+// 	makeTest: function () {
+// 		console.log('Test');
+// 	}
+// };
+
+// options.makeTest();
+
+// console.log(Object.keys(options));
+// console.log(Object.keys(options).length);
+
+
+// console.log(options["colors"]["border"]);
+
+// console.log(options.name);
+
+// delete options.name;
+
+// console.log(options);
+
+// <--перебор объектов-->
+// let counter = 0;
+// for (let key in options) {  // for of!
+// 	if (typeof(options[key]) === 'object') {
+// 		for (let i in options[key]) {
+// 			console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+// 			counter++;
+// 		}
+// 	} else {
+// 		console.log(`Свойство ${key} имеет значение ${options[key]}`);
+// 		counter++;
+// 	}
+// }
+// console.log(counter);
+
+// <--Деструктуризация объектов-->
+
+// const options = {
+// 	name: 'test',
+// 	width: 1024,
+// 	height: 1024,
+// 	colors: {
+// 		border: 'black',
+// 		bg: 'red'
+// 	}
+// };
+
+// const {border, bg} = options.colors;
+// console.log(bg);
+
