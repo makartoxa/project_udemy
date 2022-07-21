580959e0757c0bdd60d5bf311a3be2804265ccaf
Anton <makar.toxa@gmail.com>
2022-07-21 12:12:05 +0300

    CallBack - функции

--- js/script.js
+++ js/script.js
@@ -926,3 +926,34 @@
 
 // writeYourGenres ();
 
+// <!-- CallBack - функции-->
+
+// function first () {
+// 	// Do domething
+// 	setTimeout(function() {
+// 		console.log(1);
+// 	}, 500);
+// }
+
+// function second() {
+// 	console.log(2);
+// }
+
+// first();
+// second();
+
+
+
+// function learnJS(lang, callback) {
+// 	console.log(`Я учу: ${lang}`);
+// 	callback();
+// }
+// // learnJS('JavaScript', function() {
+// // 	console.log('Я прошел этот урок!');
+// // });
+// function done () {
+// 	console.log('Я прошел этот урок!');
+// }
+
+// learnJS('JavaScript', done);
+
