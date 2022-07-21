f7cb2ee72db9346038f83e3697b29c96bb7c6822
Anton <makar.toxa@gmail.com>
2022-07-21 11:54:48 +0300

    Метод trim

--- js/script.js
+++ js/script.js
@@ -852,3 +852,77 @@
 
 // fib(5);
 
+
+// <!--Метод trim-->
+
+// str.trim(); убирает пробелы в начале и конце строки
+// const a = prompt('Один из последних просмотреных фильмов?', '').trim(),
+
+// let numberOfFilms; 
+
+// function start() {
+// 	numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+// 	while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+// 		numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// 	}
+// }
+
+// start();
+
+// const personalMovieDB = {
+// 	count: numberOfFilms,
+// 	movies: {},
+// 	actors: {},
+// 	genres: [],
+// 	privat: false
+// };
+
+// function rememberMyFilms() {
+// 	for (let i = 0; i < 2; i++) {
+// 		const a = prompt('Один из последних просмотреных фильмов?', '').trim(),
+// 					b = prompt('На сколько оценете его?', '');
+// 		if (a != null && b != null && a != '' & b != '' && a.length < 50) {
+// 			personalMovieDB.movies[a] = b;
+// 			console.log('done');
+// 		} else {
+// 			console.log('error');
+// 			i--;
+// 		}
+// 	}
+// }
+
+// rememberMyFilms();
+
+// function detecPersonalevel () {
+// 			if (personalMovieDB.count < 10) {
+// 				console.log("Просмотренно довольно мало фильмов");
+// 			} else if (personalMovieDB.count >= 10 && personalMovieDB < 30) {
+// 				console.log("Вы классический зритель");
+// 			} else if (personalMovieDB.count >= 30) {
+// 				console.log("Вы киноман");
+// 			} else {
+// 				console.log("Произошла ошибка");
+// 			}
+// 		}
+
+// detecPersonalevel ();
+
+// function showMyDB (hidden) {
+// 	if (!hidden) {
+// 		console.log(personalMovieDB);
+// 	}
+// }
+
+// showMyDB(personalMovieDB.privat);
+
+// function writeYourGenres () {
+// 	for (let i = 1; i <= 3; i++) {
+// 		// const genre = prompt(`Ваш любимый жанр под номером ${i}`);
+// 		// personalMovieDB.genres[i - 1] = genre;
+// 		personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+// 	}
+// }
+
+// writeYourGenres ();
+
