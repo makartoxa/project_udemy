be2a667ddd32ddb4b69404bf3f8b203a2a60543d
Anton Makarenko <makar.toxa@gmail.com>
2022-08-03 17:00:16 +0300

    Update Task 10

--- js/script.js
+++ js/script.js
@@ -1244,3 +1244,36 @@
 //         console.log(city.toLowerCase())
 //     })
 // }
+
+// <!--Task 12-->
+
+// const someString = 'This is some strange string';
+//
+// function reverse(str) {
+//
+//     if (typeof(str) !== "string" ) {
+//         return `Ошибка!`
+//     }
+//     let newStr = '';
+//     for (let i = str.length - 1; i >= 0; i--) {
+//         newStr += str[i];
+//     }
+//     return newStr;
+// }
+// reverse(someString);
+// console.log(reverse(someString));
+
+const baseCurrencies = ['USD', 'EUR'];
+const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
+
+function availableCurr(arr, missingCurr) {
+    let str = '';
+    arr.length === 0 ? str = 'Нет доступных валют' : str = 'Доступные валюты:\n';
+
+    arr.forEach(function(curr, i) {
+        if (curr !== missingCurr) {
+            str += `${curr}\n`;
+        }
+    });
+    return str;
+}
