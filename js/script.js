e1ad4993d2dfc6edd08df5d5675ca9daed8d84b1
Anton Makarenko <makar.toxa@gmail.com>
2022-08-03 17:58:32 +0300

    Update Object.create and setPrototypeOf

--- js/script.js
+++ js/script.js
@@ -1278,3 +1278,32 @@
 //     return str;
 // }
 
+<!--Основы ООП, Прототипно-ориентированое наследование-->
+
+// let str = "some";
+// let strObj = new String(str);
+
+// console.log(typeof(str));
+// console.log(typeof(strObj));
+
+// console.dir([1,2,3])
+
+
+
+// const soldier = {
+//     health: 400,
+//     armor: 100,
+//     sayHello: function() {
+//         console.log("Hello");
+//     }
+// };
+//
+// const jonh = Object.create(soldier);  //// !!!!!!!!!!
+
+// //const jonh = {
+//     health: 100
+// };
+
+// //console.log(jonh.armor);
+// Object.setPrototypeOf(jonh, soldier)  ///  !!!!!!!!!!!
+// jonh.sayHello();
