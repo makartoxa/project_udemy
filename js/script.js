3590e64504cb38b59157f1656e9e2283789c680d
Anton Makarenko <makar.toxa@gmail.com>
2022-08-03 14:45:50 +0300

    Update Task 10

--- js/script.js
+++ js/script.js
@@ -1221,3 +1221,26 @@
 //
 // showProgrammingLangs(personalPlanPeter)
 //
+
+<!--Task 11-->
+
+// const family = ['Peter', 'Ann', 'Alex', 'Linda'];
+//
+// function showFamily(arr) {
+//     if (arr.length === 0) {
+//         return `Семья пуста`
+//     }
+//     const newFamily = family.join(' ');
+//     let str = `Семья состоит из: ${newFamily}`
+//     return  str
+// }
+// showFamily(family)
+//
+//
+// const favoriteCities = ['liSBon', 'ROME', 'miLan', 'Dublin'];
+//
+// function standardizeStrings(arr) {
+//     arr.forEach(city => {
+//         console.log(city.toLowerCase())
+//     })
+// }
