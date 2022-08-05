884873c2d14582ec409a764dabbe6bc6e1ddb1a6
Anton Makarenko <makar.toxa@gmail.com>
2022-08-05 13:40:43 +0300

    Update task 14

--- js/script.js
+++ js/script.js
@@ -1429,3 +1429,28 @@
 //     }
 // };
 // isBudgetEnough(shoppingMallData);
+
+<!--Task 14-->
+//
+// const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
+//
+// function sortStudentsByGroups(arr) {
+//     arr.sort();
+//     const a = [], b = [], c = [], rest = [];
+//
+//     for (let i = 0; i < arr.length; i++) {
+//         if (i < 3) {
+//             a.push(arr[i]);
+//         } else if (i < 6) {
+//             b.push(arr[i]);
+//         } else if (i < 9) {
+//             c.push(arr[i]);
+//         } else {
+//             rest.push(arr[i]);
+//         }
+//     }
+//     return [a,b,c, `Оставшиеся студенты: ${rest.length === 0 ? '-' : rest.join(', ')}`]
+// }
+//
+// sortStudentsByGroups(students);
+// console.log(sortStudentsByGroups(students));
