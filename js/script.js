67f91d377f18eb52520196c2278eb46a0587e981
Anton Makarenko <makar.toxa@gmail.com>
2022-08-05 15:49:56 +0300

    Динамическая типизация в JS

--- js/script.js
+++ js/script.js
@@ -1454,3 +1454,56 @@
 //
 // sortStudentsByGroups(students);
 // console.log(sortStudentsByGroups(students));
+
+<!--Динамическая типизация в JS-->
+
+// // To string
+
+// // 1.
+// console.log(typeof(String(null)));
+// console.log(String(null));
+// console.log(typeof(String(4)));
+//
+// // 2.
+//
+// console.log(typeof(5 + ''));
+//
+// const num = 5;
+// console.log("http://vk.com/catalog/" + num);
+//
+// const fontsize = 26 + 'px';
+
+// // To number
+
+// // 1.
+// console.log(typeof(Number('5')));
+
+// // 2.
+// console.log(typeof(+'4'));
+//
+// // 3.
+// console.log(typeof(parseInt("15px, 10")));
+//
+// let answ = +prompt('Hello', '');
+
+// To boolean
+
+// 0, '', null, undefined,NaN;
+
+// // 1.
+
+// let switcher = null;
+// if (switcher) {
+//     console.log('Working...');
+// }
+//
+// switcher = 1;
+// if (switcher) {
+//     console.log('Working...');
+// };
+
+// // 2.
+// console.log(typeof(Boolean('4')));
+
+// // 3.
+// console.log(typeof(!!'4'));
