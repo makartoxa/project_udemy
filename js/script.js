3f7aa3dfd37a9248208dcde633821b7a660ef40d
Anton Makarenko <makar.toxa@gmail.com>
2022-08-05 11:58:41 +0300

    Update task 13

--- js/script.js
+++ js/script.js
@@ -1386,3 +1386,46 @@
 //         })
 //     }
 // };
+
+<!--Task 13-->
+
+// const shoppingMallData = {
+//     shops: [
+//         {
+//             width: 10,
+//             length: 5
+//         },
+//         {
+//             width: 15,
+//             length: 7
+//         },
+//         {
+//             width: 20,
+//             length: 5
+//         },
+//         {
+//             width: 8,
+//             length: 10
+//         }
+//     ],
+//     height: 5,
+//     moneyPer1m3: 30,
+//     budget: 50000
+// }
+//
+// function isBudgetEnough(data) {
+//     const { shops, height, moneyPer1m3, budget } = data;
+//     let shopsS = 0;
+//     shops.forEach(shop => {
+//         shopsS += (shop.length * shop.width);
+//     })
+//     const mallValue = height * shopsS;
+//     const sumMoney = mallValue * moneyPer1m3;
+//     console.log(sumMoney);
+//     if (budget >= sumMoney) {
+//         return `Бюджета достаточно`;
+//     } else if (budget < sumMoney) {
+//         return `Бюджета недостаточно`;
+//     }
+// };
+// isBudgetEnough(shoppingMallData);
