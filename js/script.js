fa0190336e856f2d0ed81d7107eb8a2b3c684ea0
Anton Makarenko <makar.toxa@gmail.com>
2022-08-08 13:06:54 +0300

    add task searech error

--- js/script.js
+++ js/script.js
@@ -1538,3 +1538,59 @@
 // const c3 = increment();debugger
 //
 // console.log(c1, c2, c3);
+
+<!--Task searech error-->
+
+// const restorantData = {
+//     menu: [
+//         {
+//             name: 'Salad Caesar',
+//             price: '14$'
+//         },
+//         {
+//             name: 'Pizza Diavola',
+//             price: '9$'
+//         },
+//         {
+//             name: 'Beefsteak',
+//             price: '17$'
+//         },
+//         {
+//             name: 'Napoleon',
+//             price: '7$'
+//         }
+//     ],
+//     waitors: [
+//         {name: 'Alice', age: 22}, {name: 'John', age: 24}
+//     ],
+//     averageLunchPrice: '20$',
+//     openNow: true
+// };
+//
+// const {openNow} = restorantData;
+// function isOpen(prop) {
+//     let answer = '';
+//     return prop ? answer = 'Открыто' : answer = 'Закрыто';
+// }
+// console.log(isOpen(openNow))
+//
+// function isAverageLunchPriceTrue(fDish, sDish, average) {
+//     if (+fDish.price.slice(0, -1) + (sDish.price) > average) {
+//         return 'Цена ниже средней';
+//     } else {
+//         return 'Цена выше средней';
+//     }
+// }
+//
+// console.log(isAverageLunchPriceTrue(restorantData.menu[0], restorantData.menu[1], restorantData.averageLunchPrice));
+//
+// function transferWaitors(data) {
+//     const copy = Object.assign({}, data);
+//
+//     copy.waitors = [{name: 'Mike', age: 32}];
+//
+//     return copy;
+// }
+//
+// transferWaitors(restorantData);
+// console.log(transferWaitors(restorantData));
