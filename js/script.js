24b57c4a73eb69647f6567c4edae589da3f5bf65
Anton Makarenko <makar.toxa@gmail.com>
2022-08-09 11:38:48 +0300

    add Get elements from pages

--- js/script.js
+++ js/script.js
@@ -1594,3 +1594,22 @@
 //
 // transferWaitors(restorantData);
 // console.log(transferWaitors(restorantData));
+
+<!--Получение элементов со страницы-->
+
+// const box = document.getElementById('box');
+// console.log(box);
+//
+// const btns = document.getElementsByTagName('button')[1];
+// console.log(btns); //console.log(btns[1]);
+//
+// const circles = document.getElementsByClassName('circle');
+// console.log(circles);
+//
+// const hearts = document.querySelectorAll('.heart');
+// hearts.forEach(item => {
+//     console.log(item);
+// });
+//
+// const oneHeart = document.querySelector('.heart');
+// console.log(oneHeart);
