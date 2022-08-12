95145afea52c4d2efb642209ab7c2fd89d526993
Anton Makarenko <makar.toxa@gmail.com>
2022-08-12 13:28:02 +0300

    add MobileVersion

--- js/script.js
+++ js/script.js
@@ -1806,3 +1806,50 @@
 //
 // console.log(factorial(4.5));
 
+// <!--события на мобильных устройствах-->
+
+// touchstart
+// touchmove
+// touchend
+// touchenter
+// touchleave
+// touchcancel
+
+// window.addEventListener('DOMContentLoaded', () => {
+//     const box = document.querySelector('.box');
+//
+//     box.addEventListener('touchstart', (e) => {
+//         e.preventDefault();
+//
+//         console.log('start');
+//         console.log(e.touches);
+//
+//     })
+
+    // box.addEventListener('touchmove', (e) => {
+    //     e.preventDefault();
+    //
+    //     console.log('move');
+    //
+    // })
+    //
+    // box.addEventListener('touchend', (e) => {
+    //     e.preventDefault();
+    //
+    //     console.log('end');
+    //
+    // })
+
+    // box.addEventListener('touchmove', (e) => {
+    //     e.preventDefault();
+    //
+    //     console.log(e.targetTouches[0].pageX);
+    //
+    // })
+// })
+
+// touches
+// targetTouches
+// changedTouches
+
+
