ad7c40fb6c1f401b44574143d2c7e80b0afba044
Anton Makarenko <makar.toxa@gmail.com>
2022-08-12 15:22:28 +0300

    add Async, defer

--- js/script.js
+++ js/script.js
@@ -1852,4 +1852,16 @@
 // targetTouches
 // changedTouches
 
+<!--Async, defer-->
+const p = document.querySelectorAll('p');
+console.log(p);
+
+function loadScript(src) {
+    const script = document.createElement('script');
+    script.src = src;
+    script.async = false;
+    document.body.append(script);
+}
+ loadScript("js/test.js");
+loadScript("js/some.js");
 
